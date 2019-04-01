9946213806af87434aff4f5c7648663a502b60b3
konul <konul.eminova39@gmail.com>
2019-04-01 14:25:06 +0400

    added network util

--- lib/data/viewmodel/viewmodel.dart
+++ lib/data/viewmodel/viewmodel.dart
@@ -7,28 +7,16 @@ import 'package:redux_sign_in/redux/action/OnLoginAction.dart';
 import 'package:redux_sign_in/redux/action/OnRegisterAction.dart';
 import 'package:redux_sign_in/redux/middleware/thunkfunction.dart';
 class ViewModel {
-  Function(UserLogin) onLogin;
-  Function(UserRegister) onRegister;
   Function(String username,String password) buildLogin;
 
-  ViewModel({this.onLogin, this.onRegister, this.buildLogin});
+  ViewModel({this.buildLogin});
 
   factory ViewModel.create(Store<AppState> store) {
-    _onLogin(UserLogin userLogin) {
-      store.dispatch(OnLoginAction(userLogin: userLogin));
-    }
-
-    _onRegister(UserRegister userRegister) {
-      store.dispatch(OnRegisterAction(userRegister: userRegister));
-    }
-
     _buildLogin(String username,String password) {
       store.dispatch(loginThunkAction(username,password));
     }
 
     return ViewModel(
-        onLogin: _onLogin,
-        onRegister: _onRegister,
         buildLogin: _buildLogin);
   }
 }
