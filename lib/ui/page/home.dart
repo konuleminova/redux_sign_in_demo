89f501e0fc327c5fc373967cd90c12b240cbe982
konul <konul.eminova39@gmail.com>
2019-04-02 12:05:06 +0400

    home part 1

--- lib/ui/page/home.dart
+++ lib/ui/page/home.dart
@@ -5,12 +5,51 @@ import 'package:redux_sign_in/data/model/user_login.dart';
 import 'package:redux_sign_in/data/viewmodel/login_viewmodel.dart';
 
 class HomePage extends StatelessWidget {
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-        body: new Container(
-      color: Colors.red,
-    ));
+      key: scaffoldKey,
+      appBar: new AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: new Icon(
+            Icons.menu,
+            color: Colors.grey,
+          ),
+          onPressed: () => scaffoldKey.currentState.openDrawer(),
+        ),
+        title: new Text("Home"),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+            Icons.search,
+          )),
+          new IconButton(icon: Icon(Icons.shopping_cart))
+        ],
+      ),
+      drawer: new Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: new Text(
+                "KOnul Eminova",
+                style: new TextStyle(fontSize: 20),
+              ),
+              accountEmail: new Text("konul.eminova@mail.sinam.net"),
+              currentAccountPicture: Image.network(
+                  'http://www.clker.com/cliparts/5/7/4/8/13099629981030824019profile.svg.med.png'),
+            ),
+            ListTile(
+              leading: Icon(Icons.category),
+              title: Text("Categories"),
+            )
+          ],
+        ),
+      ),
+      body: new Container(),
+    );
   }
 }
