aeb70974f7d469b0a90185457b3c6a45649f54c7
konul <konul.eminova39@gmail.com>
2019-04-29 17:26:20 +0400

    design part4

--- lib/ui/page/grocery_details_page.dart
+++ lib/ui/page/grocery_details_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:redux_sign_in/ui/widgets/rating_star.dart';
-import 'package:redux_sign_in/ui/widgets/list_item/test_glistitem2.dart';
+import 'package:redux_sign_in/ui/widgets/list_item/glistitem2.dart';
 import 'package:redux_sign_in/ui/widgets/gtypography.dart';
 
 class GroceryDetailsPage extends StatelessWidget {
