53ee344ade5957b433cc48bde6c528f8cf9127c8
konul <konul.eminova39@gmail.com>
2019-04-29 17:16:35 +0400

    design part4

--- lib/ui/widgets/test_glistitem2.dart
+++ lib/ui/widgets/test_glistitem2.dart
@@ -16,58 +16,62 @@ class GroceryListItemTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(12),
-      child: ListTile(
-        leading: Container(
-            height: 80.0,
-            width: 100,
-            child: Image.asset(
-              image,
-              height: 80.0,
-            )),
-        title: Container(
-          height: 110.0,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new GroceryTitle(text: title),
-              new GrocerySubtitle(text: subtitle),
-              new GrocerySubtitle(text: "1 AZN")
-            ],
-          ),
-        ),
-        trailing: Column(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.favorite_border),
-              onPressed: () {},
-            ),
-      new GestureDetector(
-        child: new Container(
-          child: new Container(
-            padding: EdgeInsets.all(8),
-            color: Colors.lightGreen,
-            child: new SizedBox(
-              child: new Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
+        margin: EdgeInsets.all(12),
+        child: Material(
+          borderRadius: BorderRadius.circular(10.0),
+          elevation: 3.0,
+          child: new Container(child: ListTile(
+
+            leading: Container(
+                height: 80.0,
+                width: 100,
+                child: Image.asset(
+                  image,
+                  height: 80.0,
+                )),
+            title: Container(
+              height: 110.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new GroceryTitle(text: title),
+                  new GrocerySubtitle(text: subtitle),
+                  new GrocerySubtitle(text: "1 AZN")
+                ],
               ),
-              height: 20,
-              width: 20,
             ),
-          ),
-        ),
-        onTap: () {
-         // setState(() {
-           /// widget.isAdded = true;
-         // });
-        },
-      )
-          ],
-        ),
-      ),
-    );
+            trailing: Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  onPressed: () {},
+                ),
+                new GestureDetector(
+                  child: new Container(
+                    child: new Container(
+                      padding: EdgeInsets.all(8),
+                      color: Colors.lightGreen,
+                      child: new SizedBox(
+                        child: new Icon(
+                          Icons.shopping_cart,
+                          color: Colors.white,
+                        ),
+                        height: 20,
+                        width: 20,
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    // setState(() {
+                    /// widget.isAdded = true;
+                    // });
+                  },
+                )
+              ],
+            ),
+          ),color: Colors.white,)
+        ));
   }
 }
 // is is Added case UI view
