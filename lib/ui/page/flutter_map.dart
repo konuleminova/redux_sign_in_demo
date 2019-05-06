85c477fb4841513e86c290fd3340b6bf855026ad
konul <konul.eminova39@gmail.com>
2019-05-06 11:46:16 +0400

    added share

--- lib/ui/page/flutter_map.dart
+++ lib/ui/page/flutter_map.dart
@@ -42,27 +42,34 @@ class _MapPage1State extends State<MapPage1> {
         GestureDetector(
             child: new Container(
               padding: EdgeInsets.all(1),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              height: MediaQuery
+                  .of(context)
+                  .size
+                  .height * 0.3,
               alignment: AlignmentDirectional.topCenter,
               color: Colors.white,
               child: new GestureDetector(
                 child: GoogleMap(
                   gestureRecognizers: Set()
                     ..add(Factory<PanGestureRecognizer>(
-                        () => PanGestureRecognizer()))
-                    ..add(Factory<VerticalDragGestureRecognizer>(
-                        () => VerticalDragGestureRecognizer())),
+                            () => PanGestureRecognizer()))..add(
+                        Factory<VerticalDragGestureRecognizer>(
+                                () => VerticalDragGestureRecognizer())),
                   onTap: (LatLng location) {
                     MapDemoPage mp = new MapDemoPage();
                     mp.showMap();
                   },
+                  polylines:setPolyLine(),
                   tiltGesturesEnabled: true,
                   markers: _markers,
                   onCameraMove: _onCameraMove,
                   onMapCreated: _onMapCreated,
                   initialCameraPosition:
-                      CameraPosition(target: _lastMapPositon, zoom: 14.00),
+                  CameraPosition(target: _lastMapPositon, zoom: 14.00),
                 ),
                 onTap: () {
                   MapDemoPage mp = new MapDemoPage();
@@ -83,24 +90,57 @@ class _MapPage1State extends State<MapPage1> {
     _mapController = controller;
   }
 
+  setPolyLine() {
+    List<LatLng> points = new List<LatLng>();
+    points.add(new LatLng(45.52309483308097, -122.67339684069155));
+    points.add(new LatLng(40.3725459, 49.8645313));
+    points.add(new LatLng(40.3716794, 49.8646172));
+    points.add(new LatLng(40.3714996, 49.8654218));
+    points.add(new LatLng(40.3716304, 49.8659475));
+    points.add(new LatLng(40.3724713, 49.8659568));
+    points.add(new LatLng(40.3734358, 49.8673086));
+    points.add(new LatLng(40.3736156, 49.8713856));
+    points.add(new LatLng(40.3746291, 49.8717075));
+    points.add(new LatLng(40.3747599, 49.8793893));
+    points.add(new LatLng(40.3739262, 49.8797755));
+    points.add(new LatLng(40.3743022, 49.884625));
+    points.add(new LatLng(40.3789773, 49.8814064));
+    points.add(new LatLng(40.3843549, 49.8799043));
+    points.add(new LatLng(40.3857114, 49.8798184));
+    points.add(new LatLng(40.3871497, 49.8796495));
+    points.add(new LatLng(40.3886207, 49.8795181));
+    points.add(new LatLng(40.3889394, 49.8765355));
+    points.add(new LatLng(40.3890619, 49.873467));
+
+    List<Polyline> _lines = <Polyline>[
+      new Polyline(
+          polylineId:new PolylineId("112"),
+          points: points, color: Colors.redAccent
+      ),
+    ];
+    Set<Polyline> plo=new Set();
+    plo.addAll(_lines);
+    return plo;
+  }
+
   void _onCameraMove(CameraPosition position) {}
 
   void getLocations() async {
     CameraPosition position =
-        new CameraPosition(target: new LatLng(21.417276, -1.279271));
+    new CameraPosition(target: new LatLng(21.417276, -1.279271));
     _onCameraMove(position);
     final query = _textEditingController.text;
     var address = await Geocoder.local.findAddressesFromQuery(query);
     var first = address.first;
     setState(() {
       _lastMapPositon =
-          new LatLng(first.coordinates.latitude, first.coordinates.longitude);
+      new LatLng(first.coordinates.latitude, first.coordinates.longitude);
       _markers.add(Marker(
           draggable: true,
           markerId: MarkerId(_lastMapPositon.toString()),
           position: _lastMapPositon,
           infoWindow:
-              InfoWindow(title: first.countryName, snippet: first.addressLine),
+          InfoWindow(title: first.countryName, snippet: first.addressLine),
           icon: BitmapDescriptor.defaultMarker));
       _mapController.animateCamera(CameraUpdate.newCameraPosition(
           new CameraPosition(target: _lastMapPositon, zoom: 14.00)));
