d96d47a599d6a06b435e01f3abdea968e81c60bd
konul <konul.eminova39@gmail.com>
2019-05-07 17:38:06 +0400

    order history part1

--- lib/ui/widgets/dialog/rating_star_dialog.dart
+++ lib/ui/widgets/dialog/rating_star_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:redux_sign_in/ui/widgets/rating_star.dart';
 
 class RatingStarDialog extends StatelessWidget {
   final image = 'assets/img/3.jpg';
@@ -8,51 +9,59 @@ class RatingStarDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SizedBox(
-        height: 370,
-        child: Dialog(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "Thank You!",
-                  style: TextStyle(color: Colors.green),
-                ),
-                Text(
-                  "Your transaction was successful",
-                  style: label,
-                ),
-                Divider(),
-                SizedBox(height: 20.0),
-                SizedBox(height: 20.0),
-                SizedBox(height: 20.0),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                      color: Colors.grey.shade300,
-                      borderRadius: BorderRadius.circular(5.0)),
-                  child: Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.green,
-                        child: Icon(Icons.account_balance_wallet),
-                      ),
-                      SizedBox(width: 10.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Credit/Debit Card"),
-                          Text(
-                            "Master Card ending ***5",
-                            style: subtitle,
-                          ),
-                        ],
-                      )
-                    ],
+      child: SingleChildScrollView(
+        child: SizedBox(
+          child: Dialog(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Thank You!",
+                    style: TextStyle(color: Colors.green),
                   ),
-                )
-              ],
+                  Text(
+                    "How do you rate our App?",
+                    style: label,
+                  ),
+                  Divider(),
+                  SizedBox(
+                    child: new Container(
+                      child: new TextField(
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 6,
+                        decoration: InputDecoration(
+                            hintText: "Write your message",
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(5.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[RatingStarWidget(5, 4, 30)],
+                        ),
+                        SizedBox(width: 10.0),
+                        CircleAvatar(
+                          backgroundColor: Colors.green,
+                          child: Icon(Icons.send),
+                        ),
+                      ],
+                    ),
+                    alignment: Alignment.centerRight,
+                  )
+                ],
+              ),
             ),
           ),
         ),
